a7b500fec394097c24571aff5e3b773776047ba5
TheDancerCodes <rojtaracha@gmail.com>
2018-03-05 12:47:37 +0300

    [FEATURE] Integrating StrictMode
    [+] StrictMode is a developer tool which detects things you might be doing by accident and brings them to your attention so you can fix them.
    
    [+] StrictMode is most commonly used to catch accidental disk or network access on the application's main thread, where UI operations are received and animations take place.
    
    [+] Keeping disk and network operations off the main thread makes for much smoother, more responsive applications. By keeping your application's main thread responsive, you also prevent ANR dialogs from being shown to users.

--- app/src/main/java/com/thedancercodes/android_code_quality/MainActivity.java
+++ app/src/main/java/com/thedancercodes/android_code_quality/MainActivity.java
@@ -1,6 +1,7 @@
 package com.thedancercodes.android_code_quality;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -36,6 +37,36 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        /*
+           BONUS: Strict Mode
+
+           StrictMode is a developer tool that helps prevent developers of your project doing any
+           accidental flash I/O or network I/O on the main thread, because this can lead to the app
+           being sluggish or unresponsive.
+
+           It also helps in preventing ANR (App Not Responding) dialogs from showing up.
+           With StrictMode issues corrected, your app will become more responsive and the user will
+           enjoy a smoother experience.
+
+         */
+
+//        if (BuildConfig.DEBUG) {
+//            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy().Builder()
+//                    .detectDiskReads()
+//                    .detectDiskWrites()
+//                    .detectNetwork()  // or .detectAll() for all detectable problems
+//                    .penaltyLog()  // Log detected violations to the system log.
+//                    .build());
+//            StrictMode.setVmPolicy(new StrictMode.VmPolicy().Builder()
+//                    .detectLeakedSqlLiteObjects()
+//                    .detectLeakedClosableObjects()
+//                    .penaltyLog()
+//                    .penaltyDeath()  // Crashes the whole process on violation.
+//                    .build());
+//
+//        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
